1be821236d301c7e9aca01d55363c498083541b1
leon <974591189@qq.com>
2017-08-23 11:22:02 +0800

    add  left alert
     add different image methord to decided  wether the kid to leave

--- app/src/main/java/com/example/leon/taitou/MainActivity.java
+++ app/src/main/java/com/example/leon/taitou/MainActivity.java
@@ -45,29 +45,30 @@ import android.view.View.OnClickListener;
 public class MainActivity extends AppCompatActivity {
 
 
-
     private CameraView cameraView;
     private TextView goodposTextView;
     private TextView leftposTextView;
     private TextView rightposTextView;
     private TextView downposTextView;
     private TextView upposTextView;
+    private TextView imagePercentView;
     private Button btnCapture;
     private Button btnSetting;
-    private MediaPlayer mPlayer;
+    private MediaPlayer mPlayerTip,mPlayerThere;
     private Timer mTimer;
     private TimerTask mTimerTask;
-    private int count=0;
-    private static boolean alert=false;
-    private static  int historyAlert=0;
+    private int count = 0;
+    private static boolean alert = false;
+    private static int historyAlert = 0;
+    private static Bitmap  prePic;
 
-    private static float[] floatValues=new float[227*227*3];
+    private static float[] floatValues = new float[227 * 227 * 3];
     private ActivityInference activityInference;
     private static final int IMAGE_MEAN = 0;
-    private static final int INPUT_SIZE=227;
+    private static final int INPUT_SIZE = 227;
     private static final float IMAGE_STD = 255;
-    private int[] intValues=new int[227*227];
-    boolean btnflag= false;
+    private int[] intValues = new int[227 * 227];
+    boolean btnflag = false;
 
     private Executor executor = Executors.newSingleThreadExecutor();
 
@@ -77,26 +78,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cameraView = (CameraView) findViewById(R.id.cameraView);
-        goodposTextView = (TextView)findViewById(R.id.goodpos_prob);
-        leftposTextView = (TextView)findViewById(R.id.leftpos_prob);
-        rightposTextView = (TextView)findViewById(R.id.rightpos_prob);
-        downposTextView = (TextView)findViewById(R.id.downpos_prob);
-        upposTextView = (TextView)findViewById(R.id.uppos_prob);
+        goodposTextView = (TextView) findViewById(R.id.goodpos_prob);
+        leftposTextView = (TextView) findViewById(R.id.leftpos_prob);
+        rightposTextView = (TextView) findViewById(R.id.rightpos_prob);
+        downposTextView = (TextView) findViewById(R.id.downpos_prob);
+        upposTextView = (TextView) findViewById(R.id.uppos_prob);
+        imagePercentView= (TextView) findViewById(R.id.imgpercent_prob);
+
         btnCapture = (Button) findViewById(R.id.btnToggleCamera);
-       // btnSetting = (Button)findViewById(R.id.btnSetting);
+        // btnSetting = (Button)findViewById(R.id.btnSetting);
 
-       // btnSetting.setOnClickListener(new OnClickListener(){
-       //     @Override
-       //     public void onClick(View v) {
-       //         // TODO Auto-generated method stub
-       //         //textView.setText("Welcome!!");
-       //         Intent intent = new Intent();
-       //         intent.setClass(MainActivity.this, SettingActivity.class);
-       //         startActivity(intent);
-       //}
-       // });
+        // btnSetting.setOnClickListener(new OnClickListener(){
+        //     @Override
+        //     public void onClick(View v) {
+        //         // TODO Auto-generated method stub
+        //         //textView.setText("Welcome!!");
+        //         Intent intent = new Intent();
+        //         intent.setClass(MainActivity.this, SettingActivity.class);
+        //         startActivity(intent);
+        //}
+        // });
 
-        mPlayer = MediaPlayer.create(this, R.raw.tip);
+        mPlayerTip = MediaPlayer.create(this, R.raw.tip);
+        mPlayerThere=MediaPlayer.create(this, R.raw.isthere);
 
         cameraView.setCameraListener(new CameraListener() {
             @Override
@@ -116,24 +120,40 @@ public class MainActivity extends AppCompatActivity {
                 //} catch (IOException e) {
                 //    e.printStackTrace();
                 //}
-
                 //Bitmap bitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
 
-                Bitmap bitmap = BitmapFactory.decodeByteArray(picture, 0, picture.length);
-                bitmap=ImageCrop(bitmap);
-                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
-                bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
 
+                Bitmap bitmap = BitmapFactory.decodeByteArray(picture, 0, picture.length);
+                bitmap = ImageCrop(bitmap);
+
+
+
+                Bitmap resizebitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+                //find diff with current pic and previous pic
+                // to find wether child is there when  difference is more then 0.4
+                if (prePic==null){prePic=resizebitmap;}
+                double imgDiff=ImgDiffPercent(resizebitmap);
+                if(imgDiff>=0.4){
+                //alert child is there?//
+                    mPlayerThere.start();
+                    }
+                else{
+                   prePic=resizebitmap;
+                }
+
+                //3D array to 1D array
+                resizebitmap.getPixels(intValues, 0, resizebitmap.getWidth(), 0, 0, resizebitmap.getWidth(), resizebitmap.getHeight());
                 for (int i = 0; i < intValues.length; ++i) {
                     final int val = intValues[i];
                     floatValues[i * 3 + 0] = (((val >> 16) & 0xFF) - IMAGE_MEAN) / IMAGE_STD;
                     floatValues[i * 3 + 1] = (((val >> 8) & 0xFF) - IMAGE_MEAN) / IMAGE_STD;
                     floatValues[i * 3 + 2] = ((val & 0xFF) - IMAGE_MEAN) / IMAGE_STD;
                 }
-                activityPrediction();
+                activityPrediction(imgDiff);
 
 
             }
+
             @Override
             public void onCameraOpened() {
                 super.onCameraOpened();
@@ -146,33 +166,33 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-         btnCapture.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 //cameraView.captureImage();
-                 if(btnflag){
-                     btnflag = false;
-                     StopMonintor();
-                     btnCapture.setText("Start Capture");
-                 }
-                 else {
-                     btnflag =true;
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //cameraView.captureImage();
+                if (btnflag) {
+                    btnflag = false;
+                    StopMonintor();
+                    btnCapture.setText("Start Capture");
+                } else {
+                    btnflag = true;
 
-                     alert=false;
-                     historyAlert=0;
-                     count=0;
-                     
-                     StartMonintor();
-                     btnCapture.setText("Stop Capture");
-                 }
+                    alert = false;
+                    historyAlert = 0;
+                    count = 0;
 
-             }
-         });
+                    StartMonintor();
+                    btnCapture.setText("Stop Capture");
+                }
+
+            }
+        });
 
         //activityInference = new ActivityInference(getApplicationContext());
         initTensorFlowAndLoadModel();
 
     }
+
     private void initTensorFlowAndLoadModel() {
         executor.execute(new Runnable() {
             @Override
@@ -186,6 +206,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -204,29 +225,30 @@ public class MainActivity extends AppCompatActivity {
         executor.execute(new Runnable() {
             @Override
             public void run() {
-                activityInference=null;
+                activityInference = null;
             }
         });
     }
-    private void activityPrediction()
-    {
+
+    private void activityPrediction(double imgPercent) {
 
 
         // Perform inference using Tensorflow
         float[] results = activityInference.getActivityProb(floatValues);
 
-         goodposTextView.setText(Float.toString(results[0]));
-         leftposTextView.setText(Float.toString(results[1]));
-         rightposTextView.setText(Float.toString(results[2]));
-         downposTextView.setText(Float.toString(results[3]));
-         upposTextView.setText(Float.toString(results[4]));
+        goodposTextView.setText(Float.toString(results[0]));
+        leftposTextView.setText(Float.toString(results[1]));
+        rightposTextView.setText(Float.toString(results[2]));
+        downposTextView.setText(Float.toString(results[3]));
+        upposTextView.setText(Float.toString(results[4]));
+        imagePercentView.setText(Double.toString(imgPercent));
 
-        if(BadPosition(results)){
-        historyAlert++;
+        if (BadPosition(results)) {
+            historyAlert++;
         }
     }
 
-    private  void  StartMonintor(){
+    private void StartMonintor() {
         //持续60秒，间隔3秒，采集1帧，如果左或下，则语音提醒；
         //语音提醒5分钟后，重新监测
         mTimer = new Timer();
@@ -234,46 +256,48 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 count++;
-                if(!alert)
-                {
+                if (!alert) {
                     cameraView.captureImage();
 
                 }
                 if (count % 6 == 0)//60秒一直不正
                 {
 
-                    if(historyAlert >= 5){
-                        mPlayer.start();
-                        alert=true;
+                    if (historyAlert >= 5) {
+                        mPlayerTip.start();
+                        alert = true;
 
                     }
-                    historyAlert=0;
+                    historyAlert = 0;
                 }
-                if((count % 30==0) & (alert))  //提醒后5分钟恢复采集
-                {alert=false;}
+                if ((count % 30 == 0) & (alert))  //提醒后5分钟恢复采集
+                {
+                    alert = false;
+                }
             }
         };
         //开始一个定时任务
         mTimer.schedule(mTimerTask, 0, 10000);
 
     }
-    private  boolean BadPosition(float[] floatArray) {
+
+    private boolean BadPosition(float[] floatArray) {
         //最大是1 or 3项
-        boolean  badflag=false;
-        if(maxValue(floatArray,1) || maxValue(floatArray,3) ) {
-            badflag=true;
-        }
-        else {
-            badflag= false;
+        boolean badflag = false;
+        if (maxValue(floatArray, 1) || maxValue(floatArray, 3)) {
+            badflag = true;
+        } else {
+            badflag = false;
         }
         return badflag;
     }
-    private boolean maxValue(float[] floatArray,int pos) {
-        boolean maxflag=true;
-        for (int i=0;i<floatArray.length;++i) {
 
-            if (floatArray[pos]-floatArray[i] < 0) {
-                maxflag=false;
+    private boolean maxValue(float[] floatArray, int pos) {
+        boolean maxflag = true;
+        for (int i = 0; i < floatArray.length; ++i) {
+
+            if (floatArray[pos] - floatArray[i] < 0) {
+                maxflag = false;
                 break;
             }
         }
@@ -286,12 +310,35 @@ public class MainActivity extends AppCompatActivity {
 
         int wh = w / 2;
 
-        int retX =w /4;
-        int retY = h/8;
+        int retX = w / 4;
+        int retY = h / 8;
 
         return Bitmap.createBitmap(bitmap, retX, retY, wh, wh, null, false);
     }
 
+    private double ImgDiffPercent(Bitmap pic) {
+
+        long diff = 0;
+        for (int y = 0; y < pic.getHeight(); y++) {
+            for (int x = 0; x < pic.getWidth(); x++) {
+                int rgb1 = pic.getPixel(x, y);
+                int rgb2 = prePic.getPixel(x, y);
+                int r1 = (rgb1 >> 16) & 0xff;
+                int g1 = (rgb1 >>  8) & 0xff;
+                int b1 = (rgb1      ) & 0xff;
+                int r2 = (rgb2 >> 16) & 0xff;
+                int g2 = (rgb2 >>  8) & 0xff;
+                int b2 = (rgb2      ) & 0xff;
+                diff += Math.abs(r1 - r2);
+                diff += Math.abs(g1 - g2);
+                diff += Math.abs(b1 - b2);
+            }
+        }
+        double n = pic.getWidth() * pic.getHeight() * 3;
+        //double p = diff / n / 255.0;
+        //System.out.println("diff percent: " + (p * 100.0));
+        return diff / n / 255.0;
+    }
     private void makeButtonVisible() {
         runOnUiThread(new Runnable() {
             @Override
