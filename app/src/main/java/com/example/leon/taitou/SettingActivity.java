94376a70e032ea283098093206bb6da17aa4b108
Vic <yunfeng.1984@gmail.com>
2017-09-17 21:48:49 +0800

    update

--- app/src/main/java/com/example/leon/taitou/SettingActivity.java
+++ app/src/main/java/com/example/leon/taitou/SettingActivity.java
@@ -10,29 +10,54 @@ import android.view.View;
 import android.content.Intent;
 import android.app.Activity;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View riseView;
+    private View leaveView;
+    private View feedbackView;
+    private View aboutView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-        Toolbar toolbar=(Toolbar)findViewById(R.id.setting_toolbar);
-        //需要调用该函数才能设置toolbar的信息
+        Toolbar toolbar = (Toolbar) findViewById(R.id.setting_toolbar);
         setSupportActionBar(toolbar);
-        //显示向上箭头
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //设置title，我直接用toolbar会出错
         getSupportActionBar().setTitle("Setting");
+
+        riseView = findViewById(R.id.setting_rise_hint);
+        leaveView = findViewById(R.id.setting_leave_hint);
+        feedbackView = findViewById(R.id.setting_feedback_hint);
+        aboutView = findViewById(R.id.setting_about_hint);
+        riseView.setOnClickListener(this);
+        leaveView.setOnClickListener(this);
+        feedbackView.setOnClickListener(this);
+        aboutView.setOnClickListener(this);
     }
 
     @Override
-      public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-
             case android.R.id.home:
                 this.finish();
                 return true;
         }
         return super.onOptionsItemSelected(item);
-            }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.equals(riseView)) {
+
+        } else if (view.equals(leaveView)) {
+
+        } else if (view.equals(feedbackView)) {
+            Intent intent = new Intent(this, FeedbackActivity.class);
+            startActivity(intent);
+        } else if (view.equals(aboutView)) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+        }
+    }
 }
